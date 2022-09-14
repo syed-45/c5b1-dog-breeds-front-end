ef80faf5eb8a088b9a9c674b9748436c43cab359
syed <user.email>
2022-09-14 17:00:50 +0100

    extractbreedFromURL function plus dogcard styling

---
+++ src/utils/extractBreedFromURL.ts
@@ -0,0 +1,17 @@
+export function extractBreedFromUrl(url: string): string {
+    const afterBreedsIndexToEnd = url.slice(30,url.length);
+    let breed = afterBreedsIndexToEnd.slice(0, afterBreedsIndexToEnd.indexOf('/'));
+    const arrayOfBreedSplitted = breed.split('-')
+    const capitalisedArray: string[] = []
+    arrayOfBreedSplitted.map((breedElement) => {
+        capitalisedArray.push( breedElement[0].toUpperCase+breedElement.slice(1,breedElement.length) )
+    })
+    const capitalisedBreedWithSpaces = capitalisedArray.join(' ')
+    
+    return breed.includes('-') ? capitalisedBreedWithSpaces : breed[0].toUpperCase() + breed.slice(1, breed.length)
+}
+
+// "https://images.dog.ceo/breeds/vizsla/n02100583_125.jpg"
+// "https://images.dog.ceo/breeds/mountain-swiss/n02107574_2325.jpg"
+
+// console.log(extractBreedFromUrl("https://images.dog.ceo/breeds/vizsla/n02100583_125.jpg"))
