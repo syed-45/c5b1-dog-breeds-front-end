a67a243602a146c2d3b464538fcd52efaa5eab3e
syed <user.email>
2022-09-15 11:03:12 +0100

    finished getDogBreedName function

---
+++ src/utils/getDogBreedName.ts
@@ -0,0 +1,8 @@
+import { extractDogBreedFromURL } from "./extractDogBreedFromURL";
+import { prettifyDogName } from "./prettifyDogName";
+
+export function getDogBreedName(url: string): string {
+  const extractedDogBreed = extractDogBreedFromURL(url)
+  const prettifiedDogBreed = prettifyDogName(extractedDogBreed)
+  return prettifiedDogBreed
+}
